079a8b90a0a35e786bc89ef86523f092981fef97
Michael Zaccari <michael.zaccari@accelecon.com>
2014-08-27 13:26:57 -0400

    Add SNMP4J imports

--- lib/snmp4jr.rb
+++ lib/snmp4jr.rb
@@ -7,3 +7,69 @@ else
   warn 'snmp4jr can only be used with JRuby'
   exit 1
 end
+
+module Snmp4JR
+  include_package 'org.snmp4j'
+
+  module Constants
+    GET          = -96
+    GETNEXT      = -95
+    GETBULK      = -91
+    INFORM       = -90
+    NOTIFICATION = -89
+    REPORT       = -88
+    RESPONSE     = -94
+    SET          = -93
+    TRAP         = -89
+    V1TRAP       = -92
+  end
+
+  module ASN1
+    include_package 'org.snmp4j.asn1'
+  end
+
+  module Event
+    include_package 'org.snmp4j.event'
+  end
+
+  module Log
+    include_package 'org.snmp4j.log'
+  end
+
+  module MP
+    Version1  = 0
+    Version2c = 1
+    Version3  = 3
+    include_package 'org.snmp4j.mp'
+  end
+
+  module Security
+    include_package 'org.snmp4j.security'
+  end
+
+  module SMI
+    include_package 'org.snmp4j.smi'
+  end
+
+  module Test
+    include_package 'org.snmp4j.test'
+  end
+
+  module Tools
+    module Console
+      include_package 'org.snmp4j.tools.console'
+    end
+  end
+
+  module Transport
+    include_package 'org.snmp4j.transport'
+  end
+
+  module Util
+    include_package 'org.snmp4j.util'
+  end
+
+  module Version
+    include_package 'org.snmp4j.version'
+  end
+end
