5b95addacfe052fa9c690dd543f64e1bb2440844
Michael Zaccari <michael.zaccari@accelecon.com>
2014-08-28 00:31:42 -0400

    Add snmp agent

---
+++ lib/snmp4jr/agent.rb
@@ -0,0 +1,11 @@
+require 'snmp4j-agent-2.2.2.jar'
+
+module SNMP4JR
+  module Agent
+    module MO
+      module Snmp
+        include_package 'org.snmp4j.agent.mo.snmp'
+      end
+    end
+  end
+end
