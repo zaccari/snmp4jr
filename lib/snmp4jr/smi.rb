a32334c1c695fb07f6f363d6edbf0e72c53ba525
Michael Zaccari <michael.zaccari@accelecon.com>
2014-08-28 00:27:31 -0400

    Add SMI pro

---
+++ lib/snmp4jr/smi.rb
@@ -0,0 +1,14 @@
+
+require 'snmp4j-smi-pro.jar'
+
+module Snmp4JR
+  module SMI
+    include_package 'com.snmp4j.smi'
+    include_package 'org.snmp4j.smi'
+
+    module Util
+      include_package 'com.snmp4j.smi.util'
+      include_package 'org.snmp4j.smi.util'
+    end
+  end
+end
